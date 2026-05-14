612b4a5a784bb687b5d48e05059a70f49c64bfe3
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2026-05-15 04:28:52 +0900

    update src

--- src/TrussC_prebuilt_jll.jl
+++ src/TrussC_prebuilt_jll.jl
@@ -1,5 +1,28 @@
 module TrussC_prebuilt_jll
 
-greet() = print("Hello World!")
+using Artifacts
+
+function get_artifact_path()
+    return artifact"TrussC_prebuilt_jll"
+end
+
+function get_lib_path()
+    libui_lib = "libTrussC.so"
+
+    @static if Sys.iswindows()
+        libui_lib = "libTrussC.dll"
+    elseif Sys.isapple()
+        libui_lib = "libTrussC.dylib"
+    else
+        libui_lib = "libTrussC.so"
+    end
+
+    p = joinpath(get_artifact_path(), "lib", libui_lib)
+    if isfile(p)
+        return p
+    else
+        return nothing
+    end
+end
 
 end # module TrussC_prebuilt_jll
